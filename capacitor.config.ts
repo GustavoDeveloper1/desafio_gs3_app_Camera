e45a12d1f73ce96e31c8861046cf3a89e9a57028
GustavoDeveloper1 <devinvestidor02@gmail.com>
2023-03-23 00:10:18 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'challenge_galery',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
